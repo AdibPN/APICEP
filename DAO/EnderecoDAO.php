a9e5a4c89affa852234faa3a7cf10c16f9b67900
AdibPN <apaulinineto@gmail.com>
2023-03-06 09:32:38 -0300

    atualiza

--- DAO/EnderecoDAO.php
+++ DAO/EnderecoDAO.php
@@ -7,24 +7,72 @@ class EnderecoDAO extends DAO
 {
     public function __construct()
     {
-        
+        parent::__construct();
     }
 
     public function selectByCep(int $cep)
     {
         $sql = "SELECT * FROM logradouro WHERE cep=?";
 
-        $smt = $this->conexao->prepare($sql);
-        $smt->binValue(1, $cep);
-        $smt->execute();
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $cep);
+        $stmt->execute();
 
-        $endereco_obj = $smt->fetchObject("APICEP\Model\EnderecoModel");
-        $endereco_obj->arr_cidades = $this->selectCidadeByUf($endereco_obj->UF);
+        $endereco_obj = $stmt->fetchObject("APICEP\Model\EnderecoModel");
+        $endereco_obj->arr_cidades = $this->selectCidadesByUf($endereco_obj->UF);
         return $endereco_obj;
     }
 
-    public function SelectCidadesByUf($uf){
+    public function selectLogradouroByBairroAndCidade(
+        string $bairro, int $id_cidade)
+    {
+        $sql = "SELECT * FROM logradouro WHERE descricao_bairro = ? AND id_cidade = ?";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $bairro);
+        $stmt->bindValue(2, $id_cidade);
+        $stmt->execute();
+
+        return $stmt->fetchAll(DAO::FETCH_CLASS);
+
+    }
+
+    public function selectCidadesByUf($uf)
+    {
+
+        $sql = "SELECT * FROM cidade WHERE uf =? ORDER BY descricao";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $uf);
+        $stmt->execute();
+
+        return $stmt->fetchAll(DAO::FETCH_CLASS);
         
+    }
+
+    public function selectBairrosByIdCidade(int $id_cidade)
+    {
+        $sql = "SELECT descricao_bairro 
+        FROM logradouro 
+        WHERE id_cidade = ? 
+        GROUP BY  descricao _bairro = ?";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $id_cidade);
+        $stmt->execute();
+
+        return $stmt->fetchAll(DAO::FETCH_CLASS);
+    }
+
+    public function selectCepByLogradourto($logradouro)
+    {
+        $sql = "SELECT * FROM logradouro
+        WHERE descricao_sem_numero LIKE :q";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->execute([':q'=>"%" . $logradouro . "%"]);
+
+        return $stmt->fetchAll(DAO::FETCH_CLASS);
+
     }
 
 }
